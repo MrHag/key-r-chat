9420c03e8acc59b6e640ca925a108086a48b2f3d
1amL0st <1amL0st>
2021-08-04 15:21:33 +0300

    Simple sign in window

--- frontend/source/views/sign-in/SignIn.tsx
+++ frontend/source/views/sign-in/SignIn.tsx
@@ -1,11 +1,52 @@
 import React from "react";
+import { Modal } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    backgroundColor: "white",
+    borderRadius: "8px",
+    outline: "none",
+    padding: "16px",
+  },
+  title: {
+    marginBottom: "32px",
+  },
+  textField: {
+    marginBottom: "32px",
+  },
+}));
 
 const SignIn: React.FC = () => {
-  console.log("Some");
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Sign in view</h1>
-    </div>
+    <Modal
+      open={true}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <div className={classes.paper}>
+        <Typography variant="h6" className={classes.title}>
+          Sign in
+        </Typography>
+        <TextField className={classes.textField} placeholder="Login" />
+        <TextField className={classes.textField} placeholder="Password" />
+        <Button variant="contained" color="secondary">
+          Login
+        </Button>
+      </div>
+    </Modal>
   );
 };
 
